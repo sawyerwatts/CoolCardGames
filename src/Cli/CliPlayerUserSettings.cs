e6ee0a0756dc7d61e4896aaba8a4517ebe21201d
Sawyer Watts <sawyer.watts@gmail.com>
2025-11-16 16:45:10 -0800

    typo

--- src/Cli/CliPlayerUserSettings.cs
+++ src/Cli/CliPlayerUserSettings.cs
@@ -4,5 +4,5 @@ namespace CoolCardGames.Cli;
 
 public class CliPlayerUserSettings
 {
-    [Range(0, 1000_5)] public int MillisecondDelayBetweenWritingMessagesToConsole { get; set; } = 300;
+    [Range(0, 1000 * 5)] public int MillisecondDelayBetweenWritingMessagesToConsole { get; set; } = 300;
 }
